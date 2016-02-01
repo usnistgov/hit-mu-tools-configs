40695b40244f04bd51562eb3b85e2136655d877e
Nicolas Crouzier <nicolas.crouzier@nist.gov>
2016-02-01 16:24:24 -0500

    pom update wit core + bean update

--- src/main/java/gov/nist/hit/loi/config/LOIWebBeanConfig.java
+++ src/main/java/gov/nist/hit/loi/config/LOIWebBeanConfig.java
@@ -7,8 +7,8 @@ import gov.nist.hit.core.hl7v2.service.HL7V2MessageParserImpl;
 import gov.nist.hit.core.hl7v2.service.HL7V2MessageValidator;
 import gov.nist.hit.core.hl7v2.service.HL7V2MessageValidatorImpl;
 import gov.nist.hit.core.hl7v2.service.HL7V2ResourcebundleLoaderImpl;
-import gov.nist.hit.core.hl7v2.service.HL7V2ValidationReportGenerator;
-import gov.nist.hit.core.hl7v2.service.HL7V2ValidationReportGeneratorImpl;
+import gov.nist.hit.core.hl7v2.service.HL7V2ValidationReportConverter;
+import gov.nist.hit.core.hl7v2.service.HL7V2ValidationReportConverterImpl;
 import gov.nist.hit.core.service.ResourcebundleLoader;
 
 import org.springframework.context.annotation.Bean;
@@ -23,9 +23,9 @@ public class LOIWebBeanConfig {
 	      return new HL7V2ResourcebundleLoaderImpl();
 	  }
 
-	 @Bean
-	  public HL7V2ValidationReportGenerator hl7v2ValidationReportGenerator() {
-	      return new HL7V2ValidationReportGeneratorImpl();
+	@Bean
+	  public HL7V2ValidationReportConverter hl7v2ValidationReportConverter() {
+	      return new HL7V2ValidationReportConverterImpl();
 	 }
 
 	  @Bean
