7f7b5ddcec5b5e5f25576fc9a18fe8c3c55a6d1d
Nicolas Crouzier <nicolas.crouzier@nist.gov>
2016-04-11 15:06:19 -0400

    pom update

--- src/main/java/gov/nist/hit/edos/config/EDOSWebBeanConfig.java
+++ src/main/java/gov/nist/hit/edos/config/EDOSWebBeanConfig.java
@@ -6,8 +6,8 @@ import gov.nist.hit.core.hl7v2.service.HL7V2MessageParserImpl;
 import gov.nist.hit.core.hl7v2.service.HL7V2MessageValidator;
 import gov.nist.hit.core.hl7v2.service.HL7V2MessageValidatorImpl;
 import gov.nist.hit.core.hl7v2.service.HL7V2ResourcebundleLoaderImpl;
-import gov.nist.hit.core.hl7v2.service.HL7V2ValidationReportGenerator;
-import gov.nist.hit.core.hl7v2.service.HL7V2ValidationReportGeneratorImpl;
+import gov.nist.hit.core.hl7v2.service.HL7V2ValidationReportConverter;
+import gov.nist.hit.core.hl7v2.service.HL7V2ValidationReportConverterImpl;
 import gov.nist.hit.core.service.ResourcebundleLoader;
 
 import org.springframework.context.annotation.Bean;
@@ -22,9 +22,9 @@ public class EDOSWebBeanConfig {
 	      return new HL7V2ResourcebundleLoaderImpl();
 	  }
 
-	 @Bean
-	  public HL7V2ValidationReportGenerator hl7v2ValidationReportGenerator() {
-	      return new HL7V2ValidationReportGeneratorImpl();
+	@Bean
+	  public HL7V2ValidationReportConverter hl7v2ValidationReportConverter() {
+	      return new HL7V2ValidationReportConverterImpl();
 	 }
 
 	  @Bean
