d1e2b285b629d0d6fdfd5d8a9c6608fe64941f6e
Crouzier, Nicolas <nicolas.crouzier@nist.gov>
2017-08-02 16:28:47 -0400

    prepare for 1.1.0 release

--- src/main/java/gov/nist/hit/edos/config/EDOSWebBeanConfig.java
+++ src/main/java/gov/nist/hit/edos/config/EDOSWebBeanConfig.java
@@ -1,25 +1,30 @@
 package gov.nist.hit.edos.config;
 
 
+import java.io.IOException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import gov.nist.hit.core.hl7v2.service.HL7V2MessageParser;
 import gov.nist.hit.core.hl7v2.service.HL7V2MessageParserImpl;
 import gov.nist.hit.core.hl7v2.service.HL7V2MessageValidator;
 import gov.nist.hit.core.hl7v2.service.HL7V2MessageValidatorImpl;
-import gov.nist.hit.core.hl7v2.service.HL7V2ResourcebundleLoaderImpl;
+import gov.nist.hit.core.hl7v2.service.HL7V2ResourceLoaderImpl;
 import gov.nist.hit.core.hl7v2.service.HL7V2ValidationReportConverter;
 import gov.nist.hit.core.hl7v2.service.HL7V2ValidationReportConverterImpl;
-import gov.nist.hit.core.service.ResourcebundleLoader;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import gov.nist.hit.core.service.ResourceLoader;
+import gov.nist.hit.core.service.exception.ProfileParserException;
 
 
 @Configuration
 public class EDOSWebBeanConfig {
 
 	@Bean
-	  public ResourcebundleLoader resourcebundleLoader() {
-	      return new HL7V2ResourcebundleLoaderImpl();
+	  public ResourceLoader resourceLoader() throws JsonProcessingException, ProfileParserException, IOException {
+	      return new HL7V2ResourceLoaderImpl();
 	  }
 
 	@Bean
